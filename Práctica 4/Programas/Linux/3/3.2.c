62660511f1e2607451484143f4c803f69e4421b8
YairCarrillo <39676878+YairCarrillo@users.noreply.github.com>
2018-10-17 13:38:15 -0500

    Update

---
+++ Práctica 4/Programas/Linux/3/3.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void){
+	int id_proc;
+	id_proc=fork();
+	if(id_proc==0){
+		printf("Soy el proceso hijo\n");
+	}
+	else{
+		printf("Soy el proceso pradre\n");
+	}
+	printf("Mensaje en ambos\n");
+	exit(0);
+}
